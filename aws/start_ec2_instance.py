8bf5af0fe5d8cb346d733d3f24cd83a0b9bb659c
NowakArtur97 <nowart97@gmail.com>
2026-01-10 11:25:46 +0100

    Use logger instead of print

--- aws/start_ec2_instance.py
+++ aws/start_ec2_instance.py
@@ -1,18 +1,22 @@
-import os, boto3
+import os, boto3, logging, json
 
 REGION = os.environ['AWS_REGION']
 EC2_INSTANCE_ID = os.environ["EC2_INSTANCE_ID"]
+
 ec2 = boto3.client('ec2', region_name=REGION)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def start_ec2_instance():
     ec2.start_instances(InstanceIds=[EC2_INSTANCE_ID])
-    print('Successfully started EC2 instance: ' + EC2_INSTANCE_ID)
+    logger.info(f'Successfully started EC2 instance: {EC2_INSTANCE_ID}')
 
 def lambda_handler(event, context):
+    logger.info(f"Received event: {json.dumps(event)}")
     try:
         start_ec2_instance()
         return "Success"
     except Exception as e:
-        print("Exception when starting EC2 instance")
-        print(e)
+        logger.info("Exception when starting EC2 instance")
+        logger.info(e)
         return "Fail"
--- aws/stop_ec2_instance.py
+++ aws/stop_ec2_instance.py
@@ -1,19 +1,23 @@
-import os, boto3
+import os, boto3, logging, json
 
 REGION = os.environ['AWS_REGION']
 EC2_INSTANCE_ID = os.environ["EC2_INSTANCE_ID"]
+
 ec2 = boto3.client('ec2', region_name=REGION)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def stop_ec2_instance():
     ec2.stop_instances(InstanceIds=[EC2_INSTANCE_ID])
-    print('Successfully stopped EC2 instance: ' + EC2_INSTANCE_ID)
+    logger.info(f'Successfully stopped EC2 instance: {EC2_INSTANCE_ID}')
 
 def lambda_handler(event, context):
+    logger.info(f"Received event: {json.dumps(event)}")
     try:
         stop_ec2_instance()
         return "Success"
     except Exception as e:
-        print("Exception when stopping EC2 instance")
-        print(e)
+        logger.info("Exception when stopping EC2 instance")
+        logger.info(e)
         return "Fail"
     
